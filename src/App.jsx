4f9889e96cbab97b521e4b6dea87dcbc2d5f5e74
thatJoosh <joshuaprice94@hotmail.com>
2023-09-25 20:03:46 -0700

    2nd commit 2nd tab done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
+
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
   return (
     <>
-      <div className="card">
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>;
+          })
+        }
       </div>
     </>
   )
