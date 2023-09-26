a3f04e2e77def75ea6b5aa63a1718fda891385be
thatJoosh <joshuaprice94@hotmail.com>
2023-09-25 20:58:23 -0700

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,31 @@ import { useState } from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+   //console.log(featuredPup)
 
   return (
     <>
+    <div className="flex">
+    <h1>Puppy Pals</h1>
       <div>
+      { featPupId && (
+        <div className='featpup'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      ) }
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
+            return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id} className='card'>{puppy.name}</p>;
           })
         }
+      </div>
       </div>
     </>
   )
